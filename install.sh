3e63e258d3046a1705e0c753ed0902f79e6fff7a
Rambo <RamboRigs90@gmail.com>
2024-08-30 16:30:05 +0000

    troubleshooting

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#RAMBO TORRENT BOX SETUP SCRIPT. [DEBIAN 9+]
-#Installs Deluge Daemon and Web-UI
+# RAMBO TORRENT BOX SETUP SCRIPT. [DEBIAN 9+]
+# Installs Deluge Daemon and Web-UI
 
-usr=$1 #username to add to deluge group (optional argument)
-delugeUsr="deluge"
+usr=$1 # username to add to deluge group (optional argument)
+delugeUsr="debian-deluged"
 
-#Bash styling
+# Bash styling
 BLUE='\033[1;36m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # no color
@@ -15,27 +15,38 @@ delay=1 # delay in seconds after showing step
 
 printf "$ST Updating OS & Installing Deluge \n $SB"
 sleep $delay
-apt update && apt upgrade -y
-apt install deluged deluge-web git pip -y
+apt update && apt dist-upgrade -y
+apt install deluged deluge-web -y
 
 printf "$ST Configuring Deluge \n $SB"
 sleep $delay
 
-#Creating deluge user and group
-adduser --system $delugeUsr
-#gpasswd -a root deluge
+# Creating deluge user and group
+adduser --system --group $delugeUsr
 
-#if [ $# -lt 1 ]; then
-#    gpasswd -a $usr deluge
-#fi
+# Check if media group exists
+if getent group "media" > /dev/null 2>&1; then
+    echo "Group 'media' already exists. Skipping step"
+else
+    groupadd media
+fi
 
-#creating download folders & setting permssions that play nice with servarr stack
+# Add deluge
+usermod -aG media $delugeUsr
+
+# Add user to deluge and media group
+if [ $# -lt 1 ]; then
+    usermod -aG media $usr
+    usermod -aG $delugeUsr $usr
+fi
+
+# Creating download folders & setting permssions that play nice with servarr stack
 mkdir /mnt/deluge
 chmod 774 /mnt/deluge
 chown -R $delugeUsr:media /mnt/deluge
 
 printf "$ST Creating system services \n $SB"
-#Creating system service for deluge
+# Creating system service for deluge
 touch /etc/systemd/system/deluged.service
 echo "[Unit]" >> /etc/systemd/system/deluged.service
 echo "Description=Deluge Bittorrent Client Daemon" >> /etc/systemd/system/deluged.service
@@ -57,11 +68,7 @@ echo " " >> /etc/systemd/system/deluged.service
 echo "[Install] " >> /etc/systemd/system/deluged.service
 echo "WantedBy=multi-user.target " >> /etc/systemd/system/deluged.service
 
-#Starting deluge service
-systemctl start deluged
-systemctl enable deluged
-
-#Creating system service for deluge web interface
+# Creating system service for deluge web interface
 touch /etc/systemd/system/deluge-web.service
 echo "[Unit]" >> /etc/systemd/system/deluge-web.service
 echo "Description=Deluge Bittorrent Client Web Interface" >> /etc/systemd/system/deluge-web.service
@@ -80,19 +87,22 @@ echo " " >> /etc/systemd/system/deluge-web.service
 echo "[Install] " >> /etc/systemd/system/deluge-web.service
 echo "WantedBy=multi-user.target " >> /etc/systemd/system/deluge-web.service
 
-printf "$ST Changing default download location \n $SB"
+#printf "$ST Changing default download location \n $SB"
 # Change the default download location
 #sed -i 's#"download_location": "/home/deluge/Downloads"#"download_location": "/mnt/deluge"#' "/home/deluge/.config/deluge/core.conf"
 #sed -i 's#"move_completed_path": "/home/deluge/Downloads"#"move_completed_path": "/mnt/deluge"#' "/home/deluge/.config/deluge/core.conf"
 #sed -i 's#"torrentfiles_location": "/home/deluge/Downloads"#"torrentfiles_location": "/mnt/deluge"#' "/home/deluge/.config/deluge/core.conf"
 
 printf "$ST Satrting services \n $SB"
-#Starting web service
+# Starting deluge service
+systemctl start deluged
+systemctl enable deluged
+# Starting web service
 systemctl start deluge-web
 systemctl enable deluge-web
 
-printf "$ST Copying ssh keys to deluge user \n $SB"
-#Copy keys over to user
+#printf "$ST Copying ssh keys to deluge user \n $SB"
+# Copy keys over to user
 #mkdir /home/deluge/.ssh
 #cp -R /root/.ssh/* /home/deluge/.ssh/
 #chown -R deluge:deluge /home/deluge/.ssh/
