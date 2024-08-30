c6e73eed1c6a873c7268cee441fd2a23a6ff5249
Rambo <RamboRigs90@gmail.com>
2024-08-30 01:19:29 +0000

    Initial upload

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Deluge-Installer
 
 Deluge install script for headless debian based linux servers.
+Installs deluge daemon and deluge web interface. 
+It also creates a service that starts at boot.
+
+## Prepare system
+
+``apt update && apt dist-upgrade -y && apt install git qemu-guest-agent -y``
+
+
+## Clone the installer
+
+``git clone https://rigslab.com/Rambo/Deluge-Installer.git``
+
+
+## Make executable
+
+``chmod +x ./Deluge-Download/install.sh``
+
+
+## Run Installer
+
+``./Deluge-Download/install.sh user``
+
+*The user argument is optional. It adds your user to the deluge group so you can manage the downloads.*
+
+Allow the default port 8112 if using a firewall
+
+Access your web ui http://server_ip:8112
+
+Default password is ``deluge``
---
+++ install.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#RAMBO TORRENT BOX SETUP SCRIPT. [DEBIAN 9+]
+#Installs Deluge Daemon and Web-UI
+
+usr=$1 #username
+
+#Make sure arguments are passed
+if [ $# -lt 1 ]; then
+    printf "Usage: install.sh user\n"
+    exit 1
+fi
+
+#Bash styling
+BLUE='\033[1;36m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # no color
+ST="\n${YELLOW}----------------------------------------------------------------------\n\n"
+SB="\n----------------------------------------------------------------------\n\n${NC}"
+delay=1 # delay in seconds after showing step
+
+printf "$ST Updating OS & Installing Deluge \n $SB"
+sleep $delay
+apt update && apt upgrade -y
+apt install deluged deluge-web git pip -y
+
+printf "$ST Configuring Deluge \n $SB"
+sleep $delay
+
+#Creating deluge user and group
+adduser --system --group deluge
+gpasswd -a root deluge
+gpasswd -a $usr deluge
+
+#creating download folders & setting perms
+chmod 774 /home/deluge
+mkdir /home/deluge/Downloads
+chown -R deluge:deluge /home/deluge/Downloads
+
+#Creating system service for deluge
+touch /etc/systemd/system/deluged.service
+echo "[Unit]" >> /etc/systemd/system/deluged.service
+echo "Description=Deluge Bittorrent Client Daemon" >> /etc/systemd/system/deluged.service
+echo "After=network-online.target" >> /etc/systemd/system/deluged.service
+echo " " >> /etc/systemd/system/deluged.service
+echo "[Service]" >> /etc/systemd/system/deluged.service
+echo "Type=simple" >> /etc/systemd/system/deluged.service
+echo "User=deluge" >> /etc/systemd/system/deluged.service
+echo "Group=deluge" >> /etc/systemd/system/deluged.service
+echo "UMask=007" >> /etc/systemd/system/deluged.service
+echo " " >> /etc/systemd/system/deluged.service
+echo "ExecStart=/usr/bin/deluged -d" >> /etc/systemd/system/deluged.service
+echo "" >> /etc/systemd/system/deluged.service
+echo "Restart=on-failure" >> /etc/systemd/system/deluged.service
+echo " " >> /etc/systemd/system/deluged.service
+echo "# Configures the time to wait before service is stopped forcefully." >> /etc/systemd/system/deluged.service
+echo "TimeoutStopSec=300" >> /etc/systemd/system/deluged.service
+echo " " >> /etc/systemd/system/deluged.service
+echo "[Install] " >> /etc/systemd/system/deluged.service
+echo "WantedBy=multi-user.target " >> /etc/systemd/system/deluged.service
+
+#Starting deluge service
+systemctl start deluged
+systemctl enable deluged
+
+#Creating system service for deluge web interface
+touch /etc/systemd/system/deluge-web.service
+echo "[Unit]" >> /etc/systemd/system/deluge-web.service
+echo "Description=Deluge Bittorrent Client Web Interface" >> /etc/systemd/system/deluge-web.service
+echo "After=network-online.target" >> /etc/systemd/system/deluge-web.service
+echo " " >> /etc/systemd/system/deluge-web.service
+echo "[Service]" >> /etc/systemd/system/deluge-web.service
+echo "Type=simple" >> /etc/systemd/system/deluge-web.service
+echo "User=deluge" >> /etc/systemd/system/deluge-web.service
+echo "Group=deluge" >> /etc/systemd/system/deluge-web.service
+echo "UMask=027" >> /etc/systemd/system/deluge-web.service
+echo " " >> /etc/systemd/system/deluge-web.service
+echo "ExecStart=/usr/bin/deluge-web -d" >> /etc/systemd/system/deluge-web.service
+echo "" >> /etc/systemd/system/deluge-web.service
+echo "Restart=on-failure" >> /etc/systemd/system/deluge-web.service
+echo " " >> /etc/systemd/system/deluge-web.service
+echo "[Install] " >> /etc/systemd/system/deluge-web.service
+echo "WantedBy=multi-user.target " >> /etc/systemd/system/deluge-web.service
+
+#Starting web service
+systemctl start deluge-web
+systemctl enable deluge-web
+
+if [ -f /usr/bin/pushover ]; then
+    pushover "Deluge Setup Complete"
+fi
+
+printf "\n${BLUE}----------------------------------------------------------------------\n\n"
+printf "Setup Complete! \n$SB"
