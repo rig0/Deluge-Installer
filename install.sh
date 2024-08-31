8f983f6da8879234092bcf8c506f1aea2698af00
Rambo <RamboRigs90@gmail.com>
2024-08-31 15:15:19 +0000

    added more comments and terminal output

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # RAMBO TORRENT BOX SETUP SCRIPT. [DEBIAN 9+]
-# Installs Deluge Daemon and Web-UI
+# Installs Deluge Daemon and Web-UI, Adjusts permissions to play nice with Servarr stack, Edits services to comply with permissions & pre configures download location.
 
 usr=$1 # username to add to deluge group (optional argument)
 delugeUsr="debian-deluged" # the user the deluge pkg creates and uses
@@ -11,14 +11,16 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # no color
 ST="\n${YELLOW}----------------------------------------------------------------------\n\n"
 SB="\n----------------------------------------------------------------------\n\n${NC}"
-delay=1 # delay in seconds after showing step
+delay=2 # delay in seconds after showing step
 
 printf "$ST Updating OS & Installing Deluge \n $SB"
 sleep $delay
+
 apt update && apt dist-upgrade -y
 apt install deluged deluge-web -y
 
-printf "$ST Configuring Deluge \n $SB"
+
+printf "$ST Configuring Deluge user and group permissions \n $SB"
 sleep $delay
 
 # Check if media group exists
@@ -26,53 +28,76 @@ if getent group "media" > /dev/null 2>&1; then
     echo "Group 'media' already exists. Skipping step"
 else
     groupadd media
+    echo "Created 'media' group"
 fi
 
 # Add deluge user to media group
 usermod -aG media $delugeUsr
+echo "Added '$delugeUsr' to the 'media' group"
 
-# Add user to deluge and media group
+# Add our user to deluge and media group
 if [ $# -ge 1 ]; then 
     usermod -aG media $usr
     usermod -aG $delugeUsr $usr
+    echo "Added '$usr' and '$delugeUsr' to the 'media' group"
 fi
 
-# Creating download folders & setting permssions that play nice with servarr stack
+
+printf "$ST Configuring Deluge download folder \n $SB"
+sleep $delay
+
+# Creating download folders
 if [ ! -d "/mnt/deluge" ]; then
-    mkdir /mnt/deluge    
+    mkdir /mnt/deluge
+    echo "Created download folder /mnt/deluge"
+else
+    echo "Download folder /mnt/deluge already exists. Skipping step."
 fi
+
+# Setting permssions that play nice with servarr stack
 chmod 774 /mnt/deluge
 chown -R $delugeUsr:media /mnt/deluge
+echo "Adjusted download folder permissions"
 
+
+printf "$ST Editing service files with correct permissions \n $SB"
 sleep $delay
+
+#stop services
 systemctl stop deluged
 systemctl stop deluge-web
+echo "Stopped services"
 
-printf "$ST Editing service to to download fils with correct permissions \n $SB"
-sleep $delay
 # Edit the init.d script
 sed -i '/^MASK=/s/0027/0002/' "/etc/init.d/deluged" 
 sed -i '/^USER=/a GROUP=media' "/etc/init.d/deluged"
+echo "Edited /etc/init.d/deluged"
 
 # Edit the daemon service
 daemonService='/lib/systemd/system/deluged.service'
 if [ -f $daemonService ]; then
     sed -i '/^UMask=/s/007/002/' $daemonService
-    sed -i '/^Group=/s/debian-deluged/media/' $daemonService 
+    sed -i '/^Group=/s/debian-deluged/media/' $daemonService
+    echo "Edited $daemonService"
 fi
 # Edit the web service
 webService='/lib/systemd/system/deluge-web.service'
 if [ -f $webService ]; then
     sed -i '/^UMask=/s/007/002/' $webService
     sed -i '/^Group=/s/debian-deluged/media/' $webService
+    echo "Edited $webService"
 fi
 
-printf "$ST Changing default download location \n $SB"
+
+printf "$ST Changing default download location in deluge config file \n $SB"
 sleep $delay
+
 # Change the default download location
 sed -i 's#"download_location": "/var/lib/deluged/Downloads"#"download_location": "/mnt/deluge"#' "/var/lib/deluged/config/core.conf"
 sed -i 's#"move_completed_path": "/var/lib/deluged/Downloads"#"move_completed_path": "/mnt/deluge"#' "/var/lib/deluged/config/core.conf"
 sed -i 's#"torrentfiles_location": "/var/lib/deluged/Downloads"#"torrentfiles_location": "/mnt/deluge"#' "/var/lib/deluged/config/core.conf"
+echo "Edited config file"
+
 
 printf "$ST Starting daemon service \n $SB"
 sleep $delay
@@ -84,17 +109,21 @@ systemctl daemon-reload
 systemctl start deluged
 systemctl status deluged --no-pager
 
+
 printf "$ST Starting web service \n $SB"
 sleep $delay
+
 # Starting web service
 systemctl start deluge-web
 systemctl status deluge-web --no-pager
 
 # Check if UFW is installed
+echo "Checking for ufw"
 if command -v ufw > /dev/null 2>&1; then
     # Check if UFW is enabled
     if sudo ufw status | grep -q "Status: active"; then
         ufw allow 8112/tcp
+        echo "Opened delude web ui port 8112"
     fi
 fi
 
